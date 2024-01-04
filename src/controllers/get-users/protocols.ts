e1040a56193a4cda1ab2b34ed50c87d0cd93c96f
caio <caiomoitinho5@gmail.com>
2024-01-04 17:34:40 -0300

    feat: add GetUsersController

---
+++ src/controllers/get-users/protocols.ts
@@ -0,0 +1,9 @@
+import { User } from "../../models/users";
+
+export interface IGetUsersController {
+    handle(): Promise<HttpResponse<User[]>>;
+}
+
+export interface IGetUsersRepository {
+    getUsers(): Promise<User[]>
+}
